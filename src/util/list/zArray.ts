f812d501e9e9cd689c297f3030bab3c21dd234a7
xkatianx <23047821+xkatianx@users.noreply.github.com>
2024-09-20 14:16:27 +0800

    add KMP

--- src/util/list/zArray.ts
+++ src/util/list/zArray.ts
@@ -1,3 +1,23 @@
+/** KMP LPS table
+ *
+ * `return[i]` is the maximum `k` such that `str[0..k] == str[i-k+1..=i]`,
+ * except that `return[0] == 0`
+ *
+ * 🚀 linear time
+ *
+ * 🤏 linear space
+ */
+export function kmp(str: string) {
+  const dp = [0];
+  let last = 0;
+  for (let i = 1; i < str.length; i++) {
+    while (last > 0 && str[last] !== str[i]) last = dp[last - 1];
+    if (str[last] === str[i]) last++;
+    dp.push(last);
+  }
+  return dp;
+}
+
 /** Gusfield's algorithm / Z algorithm
  *
  * `return[i]` is the maximum `k` such that `str[0..k] == str[i..i+k]`,
