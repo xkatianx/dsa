72171d0ad9dc0b21b2da0f50a926b941dd9a5147
katian <23047821+xkatianx@users.noreply.github.com>
2025-09-26 13:53:55 +0800

    fix: clarify parameter naming in MonotonicStack class
    - Update the `onPop` callback parameter from `pushed` to `pushing` for better clarity in the MonotonicStack class.
    - This change enhances the readability of the code by providing a more descriptive name for the parameter being pushed onto the stack.
    
    These modifications improve code clarity and maintainability.

--- src/util/list/monotonicStack.ts
+++ src/util/list/monotonicStack.ts
@@ -1,7 +1,7 @@
 import { Stack } from "./stack";
 
 export class MonotonicStack<T> extends Stack<T> {
-  onPop?: (pushed: T, popped: T) => void;
+  onPop?: (pushing: T, popped: T) => void;
 
   constructor(private popCondition: (newVal: T, oldVal: T) => boolean) {
     super();
